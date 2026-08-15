861c8a734801c04e4ec5b88e78991c55e40d7bfe
Seto <setojpno@gmail.com>
2026-08-15 14:27:43 +0700

    Changes to be committed:

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("Seto's website loaded.");
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+    document.body.classList.add("light");
+    themeToggle.textContent = "🌙";
+} else {
+    themeToggle.textContent = "☀️";
+}
+
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("light");
+
+    const isLight = document.body.classList.contains("light");
+
+    if (isLight) {
+        themeToggle.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    } else {
+        themeToggle.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    }
+
+});
